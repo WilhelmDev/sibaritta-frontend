630a907117d020f9a0d3ac98dc018bf8814cfddd
José Vielma <josevglod@gmail.com>
2024-04-24 13:40:54 -0400

    add reservation information interface

--- interface/reservacion.ts
+++ interface/reservacion.ts
@@ -39,6 +39,28 @@ export interface IDetalle {
   calification_number?: any;
 }
 
-
+export interface ReservationInformation {
+  id: number;
+  user_name: string;
+  user_email: string;
+  user_address: string | null;
+  order_comments: string | null;
+  order_seats: string | null;
+  order_check_terms: boolean | null;
+  order_check_get_more_info: boolean | null;
+  order_paymentAt: Date;
+  order_code: string | null;
+  order_hours_cancelation: number | null;
+  order_total: number;
+  status: 'created' | 'pending' | 'completed' | 'cancelled';
+  createdAt: Date;
+  updatedAt: Date;
+  experience_name: string;
+  experience_calification: number;
+  event_date: string;
+  event_hour: number;
+  event_minute: number;
+  extras: string[];
+}
 
 export interface IImage {}
