5adbf08a6741c739ce62dadbc600ed0af018f44f
Daikone <eduardoarzolay@gmail.com>
2024-05-06 20:37:01 -0400

    feat(suggestions list): add ViewAndEdit modal (not clean impl)

--- pages/lista_suggerencias/index.tsx
+++ pages/lista_suggerencias/index.tsx
@@ -5,12 +5,41 @@ import React, { FormEvent, useEffect, useRef, useState } from "react";
 import Modal from "@/components/molecules/Modal";
 import Image from "next/image";
 
+interface ImageFromBackend {
+  id: number;
+  filename: string;
+  mimetype: string;
+  resolution: string;
+  alt: string;
+  description: string;
+  path: string;
+  fk_suggestion_id: number;
+  fk_user_id: number;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
 interface Suggestion {
   id: number;
   name: string;
   description: string;
   price: number;
-  img: Blob;
+  img: string;
+  alt: string;
+}
+
+interface SuggestionResponse {
+  success: boolean;
+  total: number;
+  data: {
+    id: number;
+    name: string;
+    description: string;
+    regular_price: string;
+    status: string;
+    fk_partner_id: number;
+    images: ImageFromBackend[];
+  }[];
 }
 
 type CreateModalSuggestion = FormData & {
@@ -37,11 +66,24 @@ export default function Index() {
     useState<boolean>(false);
   const [showCreateSuggestionModal, setShowCreateSuggestionModal] =
     useState<boolean>(false);
+  const [showViewAndEditSuggestionModal, setShowViewAndEditSuggestionModal] =
+    useState<boolean>(false);
   const [chosenSuggestion, setChosenSuggestion] = useState<Suggestion | null>(
     null
   );
   const [newSuggestionFormData, setNewSuggestionFormData] =
     useState<CreateModalSuggestion | null>(null);
+  const [nameDisabled, setNameDisabled] = useState<boolean>(true);
+  const [descriptionDisabled, setDescriptionDisabled] = useState<boolean>(true);
+  const [priceDisabled, setPriceDisabled] = useState<boolean>(true);
+
+  const openViewAndEditSuggestionModal = () => {
+    setShowViewAndEditSuggestionModal(true);
+  };
+
+  const closeViewAndEditSuggestionModal = () => {
+    setShowViewAndEditSuggestionModal(false);
+  };
 
   const openCreateSuggestionModal = () => {
     setShowCreateSuggestionModal(true);
@@ -152,6 +194,63 @@ export default function Index() {
     }
   };
 
+  const getPartnerSuggestion = async (
+    suggestionId: number
+  ): Promise<Suggestion | undefined> => {
+    try {
+      const storedPartnerId = localStorage.getItem("fk_partner_id");
+      const partnerId = storedPartnerId ? parseInt(storedPartnerId, 10) : null;
+
+      if (partnerId == null) {
+        throw Error("No partner id found");
+      }
+
+      fetch(`http://localhost:8000/api/v1/suggestion_partner/${partnerId}`, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      })
+        .then((response) => response.json())
+        .then((suggestionsResponseJson: SuggestionResponse) => {
+          console.log(suggestionsResponseJson);
+          const suggestionResponseJson = suggestionsResponseJson.data.find(
+            (suggestion) => suggestion.id === suggestionId
+          );
+
+          if (suggestionResponseJson == null) {
+            throw Error("No suggestion found with id:" + suggestionId);
+          }
+
+          const largeImage = suggestionResponseJson.images.find(
+            (image) => image.resolution === "large"
+          );
+
+          let img = suggestionResponseJson.images[0].path;
+          let alt = suggestionResponseJson.images[0].alt;
+
+          if (largeImage) {
+            img = largeImage.path;
+            alt = largeImage.alt;
+          }
+
+          const suggestion: Suggestion = {
+            id: suggestionResponseJson.id,
+            name: suggestionResponseJson.name,
+            description: suggestionResponseJson.description,
+            price: parseFloat(suggestionResponseJson.regular_price),
+            img,
+            alt,
+          };
+
+          setChosenSuggestion(suggestion);
+        });
+    } catch (e) {
+      console.log(e);
+    }
+    return;
+  };
+
   useEffect(() => {
     getPartnerSuggestions();
   }, [suggestionsAmount]);
@@ -182,7 +281,8 @@ export default function Index() {
                 <div className="suggestion-asset_btns_all">
                   <button
                     onClick={() => {
-                      setViewSuggestionsModal(true), setDataModal(sugges);
+                      openViewAndEditSuggestionModal();
+                      getPartnerSuggestion(sugges?.id);
                     }}
                   >
                     Ver
@@ -248,6 +348,99 @@ export default function Index() {
           onClick={closeCreateSuggestionModal}
         >
           Cancelar
+        </button>
+      </Modal>
+
+      {/* ViewAndEditSuggestionModal */}
+      <Modal
+        visible={showViewAndEditSuggestionModal}
+        closeModal={closeViewAndEditSuggestionModal}
+      >
+        <form
+          encType="multipart/form-data"
+          style={{ fontSize: 16 }}
+          onSubmit={(e) => alert("Actualizar sugerencia :D")}
+        >
+          <div>
+            <Image
+              src={chosenSuggestion?.img!}
+              alt={chosenSuggestion?.alt!}
+              width={300}
+              height={300}
+            />
+            {/* <label>
+              Imagen
+              <input type="file" name="img" disabled />
+            </label> */}
+          </div>
+          <label>
+            Nombre
+            <input
+              type="text"
+              name="suggestionName"
+              placeholder={chosenSuggestion?.name}
+              disabled={nameDisabled}
+            />
+            <button
+              type="button"
+              onClick={() => setNameDisabled((previous) => !previous)}
+            >
+              <Image
+                src="/partners/lapiz.png"
+                alt="Editar campo"
+                width={20}
+                height={20}
+              />
+            </button>
+          </label>
+          <label>
+            Descripción
+            <input
+              type="text"
+              name="description"
+              placeholder={chosenSuggestion?.description}
+              disabled={descriptionDisabled}
+            />
+            <button
+              type="button"
+              onClick={() => setDescriptionDisabled((previous) => !previous)}
+            >
+              <Image
+                src="/partners/lapiz.png"
+                alt="Editar campo"
+                width={20}
+                height={20}
+              />
+            </button>
+          </label>
+          <label>
+            Precio
+            <input
+              type="text"
+              name="price"
+              placeholder={chosenSuggestion?.price.toString()}
+              disabled={priceDisabled}
+            />
+            <button
+              type="button"
+              onClick={() => setPriceDisabled((previous) => !previous)}
+            >
+              <Image
+                src="/partners/lapiz.png"
+                alt="Editar campo"
+                width={20}
+                height={20}
+              />
+            </button>
+          </label>
+          {/* <button>Guardar</button> */}
+        </form>
+        <button
+          style={{ fontSize: 16 }}
+          type="button"
+          onClick={closeViewAndEditSuggestionModal}
+        >
+          Cerrar
         </button>
       </Modal>
     </div>
