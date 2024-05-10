a845afc124b6cf7dd09a4b9da93fd1828525f402
Daikone <eduardoarzolay@gmail.com>
2024-05-09 20:19:11 -0400

    feat(partner suggestions service): change route to get all partner suggestions

--- services/partner/partnerSugesstion.service.ts
+++ services/partner/partnerSugesstion.service.ts
@@ -3,7 +3,7 @@ import getConfig from '@/utils/getConfig';
 
 
 const allSuggestionPartner = async (id:any,pages:any,sizes:any) => {
-    const getSuggetions = "v1/suggestion_partner/"+id + "?page=" + pages + "&size=" + sizes;
+    const getSuggetions = "v1/all_suggestion_partner/"+id + "?page=" + pages + "&size=" + sizes;
   
      try {
         const result = await baseApi.get(getSuggetions, getConfig());
