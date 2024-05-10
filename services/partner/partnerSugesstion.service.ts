4b4af3ce06d92b676e69d2c66f2690751ed5e94b
Daikone <eduardoarzolay@gmail.com>
2024-05-09 22:08:07 -0400

    feat(suggestions services): undo previous change and make another service to get all active and inactive suggestions
    Getting a fetch failed error on console when using this endpoint, code DEPTH_ZERO_SELF_SIGNED_CERT, but everything works correctly in the interface and data is being correctly retrieved

--- services/partner/partnerSugesstion.service.ts
+++ services/partner/partnerSugesstion.service.ts
@@ -1,9 +1,9 @@
-import { baseApi } from '@/lib/baseApi';
+import { baseApi, generalBaseApi } from '@/lib/baseApi';
 import getConfig from '@/utils/getConfig';
 
 
 const allSuggestionPartner = async (id:any,pages:any,sizes:any) => {
-    const getSuggetions = "v1/all_suggestion_partner/"+id + "?page=" + pages + "&size=" + sizes;
+    const getSuggetions = "v1/suggestion_partner/"+id + "?page=" + pages + "&size=" + sizes;
   
      try {
         const result = await baseApi.get(getSuggetions, getConfig());
@@ -13,5 +13,16 @@ const allSuggestionPartner = async (id:any,pages:any,sizes:any) => {
      }
 }
 
+const activeAndInactiveSuggestionPartner = async (id:any,pages:any,sizes:any) => {
+    const getSuggetions = "v1/all_suggestion_partner/"+id + "?page=" + pages + "&size=" + sizes;
+  
+     try {
+        const result = await generalBaseApi.get(getSuggetions, getConfig());
+        return result.data
+     } catch (error) {
+        throw error;
+     }
+}
 
-export { allSuggestionPartner };
+
+export { allSuggestionPartner, activeAndInactiveSuggestionPartner };
