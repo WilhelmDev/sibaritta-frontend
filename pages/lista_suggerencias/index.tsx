7161cfdc8e643a30efb61ca6e9d669c578514fd7
Daikone <eduardoarzolay@gmail.com>
2024-05-14 07:08:51 -0400

    feat(suggestions list): add CreateSuggestionModal and ViewAndEditSuggestionModal to suggestions list page, and the reverseSuggestionStatus function

--- pages/lista_suggerencias/index.tsx
+++ pages/lista_suggerencias/index.tsx
@@ -1,265 +1,112 @@
 "use client";
-import ModalSugesstions from "@/components/partner/modals/ModalSugesstions";
-import { allSuggestionPartner } from "@/services/partner/partnerSugesstion.service";
-import React, { FormEvent, useEffect, useRef, useState } from "react";
-import Modal from "@/components/molecules/Modal";
-import Image from "next/image";
-
-interface ImageFromBackend {
-  id: number;
-  filename: string;
-  mimetype: string;
-  resolution: string;
-  alt: string;
-  description: string;
-  path: string;
-  fk_suggestion_id: number;
-  fk_user_id: number;
-  createdAt: Date;
-  updatedAt: Date;
-}
-
-interface Suggestion {
-  id: number;
-  name: string;
-  description: string;
-  price: number;
-  img: string;
-  alt: string;
-}
-
-interface SuggestionResponse {
-  success: boolean;
-  total: number;
-  data: {
-    id: number;
-    name: string;
-    description: string;
-    regular_price: string;
-    status: string;
-    fk_partner_id: number;
-    images: ImageFromBackend[];
-  }[];
-}
-
-type CreateModalSuggestion = FormData & {
-  name: string;
-  description: string;
-  price: number;
-  img: Blob;
-  partnerId: number;
-};
-
-// const mockedSuggestion: Suggestion = {
-//   id: 1,
-//   name: "Sugerencia 1",
-//   description: "Descripcion de la sugerencia 1",
-//   price: 100,
-//   img: "/prueba.jpg",
-// };
+import React, { useEffect, useRef, useState } from "react";
+import { activeAndInactiveSuggestionPartner } from "@/services/partner/partnerSugesstion.service";
+import { useCreateSuggestionModal } from "@/hook/partner/suggestions/useCreateSuggestionModal";
+import { useViewAndEditSuggestionModal } from "@/hook/partner/suggestions/useViewAndEditSuggestionModal";
+import { Suggestion } from "@/interface/suggestion.interface";
+import { getPartnerSuggestion } from "@/services/partner/getPartnerSuggestion.service";
+import ViewAndEditSuggestionModal from "@/components/partner/modals/ViewAndEditSuggestionModal";
+import CreateSuggestionModal from "@/components/partner/modals/CreateSuggestionModal";
 
 export default function Index() {
   const [suggestions, setSuggestions] = useState<any>();
-  const [dataModal, setDataModal] = useState<any>();
   const [suggestionsAmount, setSuggestionsAmount] = useState<number>(3);
-  const [viewSuggestionsModal, setViewSuggestionsModal] =
-    useState<boolean>(false);
-  const [showCreateSuggestionModal, setShowCreateSuggestionModal] =
-    useState<boolean>(false);
-  const [showViewAndEditSuggestionModal, setShowViewAndEditSuggestionModal] =
-    useState<boolean>(false);
   const [chosenSuggestion, setChosenSuggestion] = useState<Suggestion | null>(
     null
   );
-  const [newSuggestionFormData, setNewSuggestionFormData] =
-    useState<CreateModalSuggestion | null>(null);
-  const [nameDisabled, setNameDisabled] = useState<boolean>(true);
-  const [descriptionDisabled, setDescriptionDisabled] = useState<boolean>(true);
-  const [priceDisabled, setPriceDisabled] = useState<boolean>(true);
+  const {
+    showCreateSuggestionModal,
+    openCreateSuggestionModal,
+    closeCreateSuggestionModal,
+    handleCreateSuggestionModalSubmit,
+  } = useCreateSuggestionModal();
+  const {
+    showViewAndEditSuggestionModal,
+    openViewAndEditSuggestionModal,
+    closeViewAndEditSuggestionModal,
+    handleEditSuggestionModalSubmit,
+  } = useViewAndEditSuggestionModal(chosenSuggestion!);
+  const isFirstRender = useRef(true);
+  const showCreateModalPreviousValue = useRef(false);
+  const showViewAndEditModalPreviousValue = useRef(false);
 
-  const openViewAndEditSuggestionModal = () => {
-    setShowViewAndEditSuggestionModal(true);
-  };
-
-  const closeViewAndEditSuggestionModal = () => {
-    setShowViewAndEditSuggestionModal(false);
-  };
-
-  const openCreateSuggestionModal = () => {
-    setShowCreateSuggestionModal(true);
-  };
-
-  const closeCreateSuggestionModal = () => {
-    setShowCreateSuggestionModal(false);
-  };
-
-  const handleCreateSuggestionModalSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    const formData = new FormData(e.currentTarget);
-
-    const { suggestionName, description, price, img } =
-      Object.fromEntries(formData);
-
-    const newSuggestionFormData = {
-      ...formData,
-      name: suggestionName as string,
-      description: description as string,
-      price: parseFloat(price as string),
-      img: img as Blob,
-      partnerId: parseInt(localStorage.getItem("fk_partner_id") as string, 10),
-    };
-
-    // console.log(newSuggestionFormData);
-
-    setNewSuggestionFormData(newSuggestionFormData);
-  };
-
-  useEffect(() => {
-    if (newSuggestionFormData) {
-      const formData = new FormData();
-      formData.append("name", newSuggestionFormData.name);
-      formData.append("description", newSuggestionFormData.description);
-      formData.append("regular_price", newSuggestionFormData.price.toString());
-      formData.append("files", newSuggestionFormData.img);
-      formData.append(
-        "fk_partner_id",
-        newSuggestionFormData.partnerId.toString()
-      );
-      formData.append("fk_user_id", "1");
-      formData.append("alt", "alt text");
-
-      // let key: keyof typeof newSuggestionFormData;
-      // for (key in newSuggestionFormData) {
-      //   const value = newSuggestionFormData[key];
-      //   if (typeof value === "number") {
-      //     if (key === "partnerId") {
-      //       formData.append("fk_partner_id", value.toString());
-      //       continue;
-      //     }
-      //     if (key === "price") {
-      //       formData.append("regular_price", value.toString());
-      //       continue;
-      //     }
-      //     formData.append(key, value.toString());
-      //     continue;
-      //   }
-      //   if (value instanceof Blob) {
-      //     formData.append("files", value);
-      //     continue;
-      //   }
-      //   formData.append(key, value);
-      // }
-
-      console.log(Object.fromEntries(formData));
-
-      fetch(`${process.env.NEXT_PUBLIC_URL}/v1/suggestion/create`, {
-        method: "POST",
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("token")}`,
-        },
-        body: formData,
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          console.log("Success:", data);
-          getPartnerSuggestions();
-          closeCreateSuggestionModal();
-        })
-        .catch((error) => {
-          console.error(`Error: ${error}`);
-        });
-    }
-  }, [newSuggestionFormData]);
-
-  const closeViewSuggestionsModal = () => {
-    setViewSuggestionsModal(false);
-  };
-
-  const getPartnerSuggestions = async () => {
+  async function getPartnerSuggestions() {
     let partnerId = null;
     if (typeof window !== "undefined") {
       const storedPartnerId = localStorage.getItem("fk_partner_id");
       partnerId = storedPartnerId ? parseInt(storedPartnerId, 10) : null;
     }
     try {
-      const { data } = await allSuggestionPartner(
+      const { data } = await activeAndInactiveSuggestionPartner(
         partnerId,
         0,
         suggestionsAmount
       );
       setSuggestions(data);
-    } catch (error) {
-      console.log(error);
+    } catch {
+      alert(
+        "Ha ocurrido un error durante la carga de las sugerencias de este partner."
+      );
     }
-  };
+  }
 
-  const getPartnerSuggestion = async (
-    suggestionId: number
-  ): Promise<Suggestion | undefined> => {
-    try {
-      const storedPartnerId = localStorage.getItem("fk_partner_id");
-      const partnerId = storedPartnerId ? parseInt(storedPartnerId, 10) : null;
-
-      if (partnerId == null) {
-        throw Error("No partner id found");
-      }
-
-      fetch(`http://localhost:8000/api/v1/suggestion_partner/${partnerId}`, {
-        method: "GET",
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("token")}`,
-        },
-      })
-        .then((response) => response.json())
-        .then((suggestionsResponseJson: SuggestionResponse) => {
-          console.log(suggestionsResponseJson);
-          const suggestionResponseJson = suggestionsResponseJson.data.find(
-            (suggestion) => suggestion.id === suggestionId
-          );
-
-          if (suggestionResponseJson == null) {
-            throw Error("No suggestion found with id:" + suggestionId);
-          }
-
-          const largeImage = suggestionResponseJson.images.find(
-            (image) => image.resolution === "large"
-          );
-
-          let img = suggestionResponseJson.images[0].path;
-          let alt = suggestionResponseJson.images[0].alt;
-
-          if (largeImage) {
-            img = largeImage.path;
-            alt = largeImage.alt;
-          }
-
-          const suggestion: Suggestion = {
-            id: suggestionResponseJson.id,
-            name: suggestionResponseJson.name,
-            description: suggestionResponseJson.description,
-            price: parseFloat(suggestionResponseJson.regular_price),
-            img,
-            alt,
-          };
-
-          setChosenSuggestion(suggestion);
-        });
-    } catch (e) {
-      console.log(e);
-    }
-    return;
+  const reverseSuggestionStatus = (
+    suggestionId: number,
+    status: "active" | "deleted"
+  ) => {
+    const formData = new FormData();
+    formData.append("status", status === "active" ? "deleted" : "active");
+    fetch(`${process.env.NEXT_PUBLIC_URL}/v1/suggestion/${suggestionId}`, {
+      method: "PUT",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+      body: formData,
+    }).then(() => getPartnerSuggestions());
   };
 
   useEffect(() => {
     getPartnerSuggestions();
   }, [suggestionsAmount]);
 
+  useEffect(() => {
+    if (isFirstRender.current) {
+      return;
+    }
+
+    showCreateModalPreviousValue.current = !showCreateSuggestionModal;
+
+    if (showCreateModalPreviousValue.current && !showCreateSuggestionModal) {
+      getPartnerSuggestions();
+    }
+  }, [showCreateSuggestionModal]);
+
+  useEffect(() => {
+    if (isFirstRender.current) {
+      return;
+    }
+
+    showViewAndEditModalPreviousValue.current = !showViewAndEditSuggestionModal;
+
+    if (
+      showViewAndEditModalPreviousValue.current &&
+      !showViewAndEditSuggestionModal
+    ) {
+      getPartnerSuggestions();
+    }
+  }, [showViewAndEditSuggestionModal]);
+
+  useEffect(() => {
+    if (isFirstRender.current) {
+      isFirstRender.current = false;
+    }
+  }, []);
+
   return (
     <div className="list_suggestions_box main-page">
       <div className="box_suggestion_list_box ">
         <div className="list_suggestion_title ">
-          <h2 className=" ">Sugerencias Sibaritta</h2>
+          <h2 className=" ">Listado de Sugerencias Sibaritta</h2>
           <button
             className="list_suggestion_btn"
             onClick={openCreateSuggestionModal}
@@ -280,17 +127,41 @@ export default function Index() {
 
                 <div className="suggestion-asset_btns_all">
                   <button
-                    onClick={() => {
+                    onClick={async () => {
+                      const storedPartnerId =
+                        localStorage.getItem("fk_partner_id");
+                      const partnerId = storedPartnerId
+                        ? parseInt(storedPartnerId, 10)
+                        : null;
+
+                      if (partnerId == null) {
+                        throw Error("No partner id found");
+                      }
+
+                      const chosenSuggestion = await getPartnerSuggestion(
+                        partnerId,
+                        sugges?.id
+                      );
+
+                      setChosenSuggestion(chosenSuggestion);
                       openViewAndEditSuggestionModal();
-                      getPartnerSuggestion(sugges?.id);
                     }}
                   >
                     Ver
                   </button>
-                  <div className="suggestion_activo ">
-                    <p>{sugges?.status}</p>
-                    <div className="w-[1rem] h-[1rem] rounded-full bg-[#07FD11] tablet:w-[1.5rem] tablet:h-[1.5rem]"></div>
-                  </div>
+                  <button
+                    className="suggestion_activo"
+                    onClick={() => {
+                      reverseSuggestionStatus(sugges?.id, sugges?.status);
+                    }}
+                  >
+                    {sugges?.status}
+                    {sugges?.status === "active" ? (
+                      <div className="w-[1rem] h-[1rem] rounded-full bg-[#07FD11] tablet:w-[1.5rem] tablet:h-[1.5rem]"></div>
+                    ) : (
+                      <div className="w-[1rem] h-[1rem] rounded-full bg-[#FD1107] tablet:w-[1.5rem] tablet:h-[1.5rem]"></div>
+                    )}
+                  </button>
                 </div>
               </div>
             </div>
@@ -306,143 +177,18 @@ export default function Index() {
         </div>
       </div>
 
-      <ModalSugesstions
-        suggestions={dataModal}
-        visible={viewSuggestionsModal}
-        closeModal={closeViewSuggestionsModal}
+      <CreateSuggestionModal
+        close={closeCreateSuggestionModal}
+        show={showCreateSuggestionModal}
+        onSubmit={handleCreateSuggestionModalSubmit}
       />
 
-      {/* CreateSuggestionModal */}
-      <Modal
-        visible={showCreateSuggestionModal}
-        closeModal={closeCreateSuggestionModal}
-      >
-        <form
-          encType="multipart/form-data"
-          style={{ fontSize: 16 }}
-          onSubmit={(e) => handleCreateSuggestionModalSubmit(e)}
-        >
-          <div>
-            <label>
-              Imagen
-              <input type="file" name="img" />
-            </label>
-          </div>
-          <label>
-            Nombre
-            <input type="text" name="suggestionName" placeholder={"Hola"} />
-          </label>
-          <label>
-            Descripción
-            <input type="text" name="description" placeholder={"Hola"} />
-          </label>
-          <label>
-            Precio
-            <input type="text" name="price" placeholder={"Hola"} />
-          </label>
-          <button>Guardar</button>
-        </form>
-        <button
-          style={{ fontSize: 16 }}
-          type="button"
-          onClick={closeCreateSuggestionModal}
-        >
-          Cancelar
-        </button>
-      </Modal>
-
-      {/* ViewAndEditSuggestionModal */}
-      <Modal
-        visible={showViewAndEditSuggestionModal}
-        closeModal={closeViewAndEditSuggestionModal}
-      >
-        <form
-          encType="multipart/form-data"
-          style={{ fontSize: 16 }}
-          onSubmit={(e) => alert("Actualizar sugerencia :D")}
-        >
-          <div>
-            <Image
-              src={chosenSuggestion?.img!}
-              alt={chosenSuggestion?.alt!}
-              width={300}
-              height={300}
-            />
-            {/* <label>
-              Imagen
-              <input type="file" name="img" disabled />
-            </label> */}
-          </div>
-          <label>
-            Nombre
-            <input
-              type="text"
-              name="suggestionName"
-              placeholder={chosenSuggestion?.name}
-              disabled={nameDisabled}
-            />
-            <button
-              type="button"
-              onClick={() => setNameDisabled((previous) => !previous)}
-            >
-              <Image
-                src="/partners/lapiz.png"
-                alt="Editar campo"
-                width={20}
-                height={20}
-              />
-            </button>
-          </label>
-          <label>
-            Descripción
-            <input
-              type="text"
-              name="description"
-              placeholder={chosenSuggestion?.description}
-              disabled={descriptionDisabled}
-            />
-            <button
-              type="button"
-              onClick={() => setDescriptionDisabled((previous) => !previous)}
-            >
-              <Image
-                src="/partners/lapiz.png"
-                alt="Editar campo"
-                width={20}
-                height={20}
-              />
-            </button>
-          </label>
-          <label>
-            Precio
-            <input
-              type="text"
-              name="price"
-              placeholder={chosenSuggestion?.price.toString()}
-              disabled={priceDisabled}
-            />
-            <button
-              type="button"
-              onClick={() => setPriceDisabled((previous) => !previous)}
-            >
-              <Image
-                src="/partners/lapiz.png"
-                alt="Editar campo"
-                width={20}
-                height={20}
-              />
-            </button>
-          </label>
-          {/* <button>Guardar</button> */}
-        </form>
-        <button
-          style={{ fontSize: 16 }}
-          type="button"
-          onClick={closeViewAndEditSuggestionModal}
-        >
-          Cerrar
-        </button>
-      </Modal>
+      <ViewAndEditSuggestionModal
+        show={showViewAndEditSuggestionModal}
+        close={closeViewAndEditSuggestionModal}
+        onSubmit={handleEditSuggestionModalSubmit}
+        chosenSuggestion={chosenSuggestion!}
+      />
     </div>
   );
 }
